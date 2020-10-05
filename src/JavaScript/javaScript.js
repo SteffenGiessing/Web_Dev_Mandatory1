331288508079a63f470d57319d994fdce8a4626c
stef2761 <Snookey1995@hotmail.com>
2020-10-05 23:21:23 +0200

    commit

--- src/JavaScript/javaScript.js
+++ src/JavaScript/javaScript.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     let searchOpt = $("#searchOption").val();
-
     let searchSpecification = $("#searchSpecification").val();
 
 
     $("#search").click(function () {
+
         console.log("Hitting?");
         const URL = "https://api.themoviedb.org/3/search/" + $("#searchOption").val() + "?api_key=a6f2492e0895b528768eb39f7815f1e4&language=en-US&page=1&include_adult=false&query=" + $("#searchSpecification").val();
 
@@ -12,33 +12,70 @@ $(document).ready(function () {
             url: URL,
             type: "GET"
         }).done(function (data) {
-            switch (searchOpt) {
-                case 'movie':
-                    data.results.forEach(element => {
-                        $("#boxes > #title").append("<p>" + element.title + "</p>").on("click", function (){
-                            //TODO
-                            // Needs to fetch specific clicked element and the rest of the element associated with it.
-                            $(".modal").css("display","block");
-                            $("#close").on("click", function (){
-                                    $(".modal").css("display", "none");
-                            });
+            if (searchOpt === 'movie') {
+                movieHandler(data);
 
+
+                /*        Object.entries(element).forEach(([key, value]) => {
+                            $("#boxes > #title").append("<p>" + key.title + "</p>")
+                            console.log(`${key.title} ${value}`)
                         });
-                        $("#boxes > #releaseDate").append("<p>" + element.release_date + "</p>");
-                        $("#boxes > #language").append("<p>" + element.original_language + "</p>");
-                        $("#modalContent > #title1").append("<p>" + element.title + "</p>");
-                    });
-                    $('#boxes').show();
-                    break;
+                          $("#boxes > #title").append("<p>" + element.title + "</p>").on("click", function (){
+                              //TODO
+                              // Needs to fetch specific clicked element and the rest of the element associated with it.
+                              $(".modal").css("display","block");
+                              $("#close").on("click", function (){
+                                      $(".modal").css("display", "none");
+                              });
+                          });
 
-                case 'person':
-
-                    break;
-
-                default:
-                    break;
+                          $("#boxes > #releaseDate").append("<p>" + element.release_date + "</p>");
+                          $("#boxes > #language").append("<p>" + element.original_language + "</p>");
+                          $("#modalContent > #title1").append("<p>" + element.title + "</p>");
+                      });*/
+                //   $('#boxes').show();
+            } else if (searchOpt === 'person') {
+            } else {
             }
 
         });
     });
+ function movieHandler(data) {
+
+     var movieResults = '';
+     data.results.forEach(element => {
+         var divId = "divId_"+element.title;
+         movieResults += '<div id="movie-info-box" class="divClass">'
+         movieResults += 'Title: <span id="title">' + element.title +'</span>';
+         movieResults += 'Release Date: <span id="release-date">' + element.release_date + '</span>';
+         movieResults += 'Language: <span id="language">' + element.original_language + '</span>';
+         movieResults += '</div>'
+         console.log("title: " + element.title);
+
+     });
+     Object.entries(data).forEach(([key, value]) => {
+         movieResults += 'Title: <span id="title">' + element.title +'</span>';
+         console.log(key, value);
+         console.log(data.indexes);
+     });
+     $('#movieContainer').html(movieResults);
+     $('#movieContainer').show();
+
+     $(".divClass").click(function (){
+         console.log("Steffen")
+                var movieResult = '';
+                movieResult +='<div id="modalDiv" class="modalDiv">'
+                movieResult += 'Title: <span id="taberSpan">'+ this.title + '</span>';
+         movieResults += '</div>'
+         $("#modal").css("display","block");
+
+         $("#close").on("click", function (){
+             $("#modal").css("display", "none");
+
+         });
+
+
+
+     });
+ }
 });
