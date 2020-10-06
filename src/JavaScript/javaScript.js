d355ac8343945f34d1987b2a34637a0113fb7bad
stef2761 <Snookey1995@hotmail.com>
2020-10-06 17:29:40 +0200

    commit

--- src/JavaScript/javaScript.js
+++ src/JavaScript/javaScript.js
@@ -14,68 +14,120 @@ $(document).ready(function () {
         }).done(function (data) {
             if (searchOpt === 'movie') {
                 movieHandler(data);
-
-
-                /*        Object.entries(element).forEach(([key, value]) => {
-                            $("#boxes > #title").append("<p>" + key.title + "</p>")
-                            console.log(`${key.title} ${value}`)
-                        });
-                          $("#boxes > #title").append("<p>" + element.title + "</p>").on("click", function (){
-                              //TODO
-                              // Needs to fetch specific clicked element and the rest of the element associated with it.
-                              $(".modal").css("display","block");
-                              $("#close").on("click", function (){
-                                      $(".modal").css("display", "none");
-                              });
-                          });
-
-                          $("#boxes > #releaseDate").append("<p>" + element.release_date + "</p>");
-                          $("#boxes > #language").append("<p>" + element.original_language + "</p>");
-                          $("#modalContent > #title1").append("<p>" + element.title + "</p>");
-                      });*/
-                //   $('#boxes').show();
             } else if (searchOpt === 'person') {
+                personHandler(data)
             } else {
             }
 
         });
     });
- function movieHandler(data) {
 
-     var movieResults = '';
-     data.results.forEach(element => {
-         var divId = "divId_"+element.title;
-         movieResults += '<div id="movie-info-box" class="divClass">'
-         movieResults += 'Title: <span id="title">' + element.title +'</span>';
-         movieResults += 'Release Date: <span id="release-date">' + element.release_date + '</span>';
-         movieResults += 'Language: <span id="language">' + element.original_language + '</span>';
-         movieResults += '</div>'
-         console.log("title: " + element.title);
-
-     });
-     Object.entries(data).forEach(([key, value]) => {
-         movieResults += 'Title: <span id="title">' + element.title +'</span>';
-         console.log(key, value);
-         console.log(data.indexes);
-     });
-     $('#movieContainer').html(movieResults);
-     $('#movieContainer').show();
-
-     $(".divClass").click(function (){
-         console.log("Steffen")
-                var movieResult = '';
-                movieResult +='<div id="modalDiv" class="modalDiv">'
-                movieResult += 'Title: <span id="taberSpan">'+ this.title + '</span>';
-         movieResults += '</div>'
-         $("#modal").css("display","block");
-
-         $("#close").on("click", function (){
-             $("#modal").css("display", "none");
-
-         });
+    function personHandler(data) {
+        console.log("HItting PErson")
+    }
 
 
+    function movieHandler(data) {
+        let composer = '';
+        let producer = '';
+        let eProducer= '';
+        let writer ='';
+        let director = '';
+        let genres = '';
+        let production_companies = '';
+        let actor = '';
+        var movieResults = '';
+        data.results.forEach(element => {
 
-     });
- }
+            movieResults += '<div id="movie-info-box" class="divClass">'
+            movieResults += '<span id="id">' + element.id + '</span>';
+            movieResults += 'Title: <span id="title">' + element.title + '</span>';
+            movieResults += 'Release Date: <span id="release-date">' + element.release_date + '</span>';
+            movieResults += 'Language: <span id="language">' + element.original_language + '</span>';
+            movieResults += '</div>'
+            console.log("title: " + element.title);
+            console.log("id: " + element.id);
+
+        });
+        $('#movieContainer').html(movieResults);
+        $('#movieContainer').show();
+
+        $(".divClass").click(function () {
+            var specificResult = '';
+            let id = $(this).find("#id").text();
+            console.log(id)
+            var ActorURL = "https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=a6f2492e0895b528768eb39f7815f1e4"
+            var IDURL = "https://api.themoviedb.org/3/movie/" + id + "?api_key=a6f2492e0895b528768eb39f7815f1e4&language=en-US"
+            $.ajax({
+                url: IDURL,
+                type: "GET"
+            }).done(function (data) {
+                $('#modalTitle').html(data.title);
+                $('#modalRelease').html(data.release_date);
+                $('#modalLanguage').html(data.original_language);
+                $('#modalRuntime').html(data.runtime);
+                $('#modalOverview').html(data.overview);
+                $('#modalLink').html(data.link);
+
+                data.genres.forEach(element => {
+                    genres += '<li>' + element.name + '</li>'
+
+                });
+                $('#modalGenre').html(genres);
+
+                data.production_companies.forEach(element => {
+                    production_companies += '<li>' + element.name + '</li>'
+                });
+                $('#modalProductionCom').html(production_companies);
+
+                $.ajax({
+                    url: ActorURL,
+                    type: "GET"
+                }).done(function (data) {
+                    data.cast.forEach(element => {
+                        actor += '<li>' + element.name + '</li>'
+                    });
+                    $('#modalActors').html(actor);
+                    data.crew.forEach(element => {
+                        if (element.job ==="Director")
+                        director += '<li>'+ element.name+'</li>'
+                    });
+                    $('#modalDirectors').html(director);
+                    data.crew.forEach(element =>{
+                       if (element.job ==="Writer")
+                           writer += '<li>'+ element.name+'</li>'
+                    });
+                    $('#modalScriptWriters').html(writer);
+                    data.crew.forEach(element => {
+                       if (element.job ==="Executive Producer")
+
+                        eProducer += '<li>' + element.name + '</li>'
+                    });
+                    $('#modalExecutives').html(eProducer);
+                    data.crew.forEach(element => {
+                        if (element.job ==="Producer")
+
+                        producer += '<li>' + element.name + '</li>'
+                    });
+                    $('#modalProducers').html(producer);
+                    data.crew.forEach(element => {
+                        if (element.job ==="Original Music Composer")
+
+                            composer += '<li>' + element.name + '</li>'
+                    });
+                    $('#modalComposer').html(composer);
+
+
+                });
+
+                $(".modal").css("display", "block");
+                $("#close").on("click", function () {
+                    $("#modal").css("display", "none");
+                });
+            });
+        });
+    }
 });
+
+
+
